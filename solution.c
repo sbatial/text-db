b0e89fe881ba2cf921b177306d7fce015cf2c7f0
sbatial <mail@kbode.com>
2023-06-04 11:19:13 +0200

    Add all known function signatures

--- solution.c
+++ solution.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char **new_database();
+void free_database(char **database);
+char **add_message(char *message, char **database, size_t *ptr_n);
+char **delete_last_message(char **database, size_t *ptr_n);
+void print_message(char **database, size_t i, size_t n);
+char **read_and_add_line(char **database, size_t *ptr_n);
+void print_all(char **database);
+
 int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
