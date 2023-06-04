755145f8bf0198896fb9cf18ae077e4eb14d08db
sbatial <mail@kbode.com>
2023-06-04 11:32:29 +0200

    Add comments to visually split up subexcercises

--- solution.c
+++ solution.c
@@ -3,13 +3,20 @@
 
 #define N_BASE 5
 
+// a
 char **new_database() { return malloc(N_BASE * sizeof(char *)); }
 
 void free_database(char **database) { free(database); }
+
+// b
 char **add_message(char *message, char **database, size_t *ptr_n);
 char **delete_last_message(char **database, size_t *ptr_n);
+
+// c
 void print_message(char **database, size_t i, size_t n);
 char **read_and_add_line(char **database, size_t *ptr_n);
+
+// d
 void print_all(char **database);
 
 int main() {
