6cc944f2140359a16635b28a6fa73a9b1b405432
sbatial <mail@kbode.com>
2023-06-05 19:02:37 +0200

    fix: double free occurs

--- solution.c
+++ solution.c
@@ -157,9 +157,10 @@ void free_all(DATABASE *database) {
     database = delete_last_message(database);
   }
 }
+
 void delete_db(DATABASE *database) {
   free_all(database);
-  free_database(database);
+  // free_database(database);
 }
 
 void print_db_row(size_t db_index, size_t longest_row, DATABASE *database) {
