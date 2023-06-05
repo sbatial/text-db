f9378857f6dc322955357a71c930c51907bb2202
sbatial <mail@kbode.com>
2023-06-05 03:59:30 +0200

    Migrate all functions to take DATABASE struct

--- solution.c
+++ solution.c
@@ -19,14 +19,15 @@ char **new_database() {
   // just allocate a wrong amount of memory whereas calloc would fail
 }
 
-void free_database(char **database) { free(database); }
+void free_database(DATABASE *database) { free(database); }
 
 // b
-char **add_message(char *message, char **database, size_t *ptr_n) {
+DATABASE *add_message(char *message, DATABASE *database) {
   unsigned long msg_len = strlen(message);
 
-  if (0 == (*ptr_n % N_BASE)) {
-    database = reallocarray(database, (*ptr_n + N_BASE), sizeof(char *));
+  if (0 == (database->msg_count % N_BASE)) {
+    database->data = reallocarray(
+        database->data, (database->msg_count + N_BASE), sizeof(char *));
     // Almost the same as:
     // database = realloc(database, (*ptr_n + N_BASE) * sizeof(char *));
     // but safer because (*ptr_n + N_BASE) * sizeof(char *) could overflow and
@@ -38,23 +39,23 @@ char **add_message(char *message, char **database, size_t *ptr_n) {
 
   strcpy(msg_store, message);
 
-  *(database + *ptr_n) = msg_store;
+  *(database->data + database->msg_count) = msg_store;
 
-  ++(*ptr_n);
+  ++database->msg_count;
 
   return database;
 }
 
-char **delete_last_message(char **database, size_t *ptr_n) {
-  --(*ptr_n);
+DATABASE *delete_last_message(DATABASE *database) {
+  --(database->msg_count);
 
-  free(*(database + *ptr_n));
+  free(*(database->data + database->msg_count));
   // == free(database[*ptr_n]);
 
-  if (0 == (*ptr_n % N_BASE)) {
+  if (0 == (database->msg_count % N_BASE)) {
     // Explicitly subtracting N_BASE again could easily lead to mis-calculations
     // but *ptr_n should always reflect the (least) size necessary
-    database = reallocarray(database, *ptr_n, sizeof(char *));
+    database = reallocarray(database, database->msg_count, sizeof(char *));
     // Probably not necessary here because we _shrink_ memory but still safer
     // than
     // database = realloc(database, *ptr_n * sizeof(char *));
@@ -77,55 +78,53 @@ void print_message(char **database, size_t i, size_t n) {
 // makes more sense to flesh it out a bit more and take into account that in an
 // array(/db) with e.g. 6 messages you can only access messages up to db[5]
 
-void print_db_message(char **database, size_t db_index, size_t message_count) {
-  if ((message_count < 1) || (db_index > (message_count - 1))) {
+void print_db_message(size_t db_index, DATABASE *database) {
+  if ((database->msg_count < 1) || (db_index > (database->msg_count - 1))) {
     printf("\033[31;mCannot access database at position %zu!\033[0;m (db_index "
            "= %zu "
            "> message_count = %zu)\n",
            db_index, db_index,
-           message_count > 0 ? message_count - 1 : message_count);
+           database->msg_count > 0 ? database->msg_count - 1
+                                   : database->msg_count);
     return;
   }
 
-  printf("Message at %zu: %s\n", db_index, database[db_index]);
+  printf("Message at %zu: %s\n", db_index, database->data[db_index]);
 }
-char **read_and_add_line(char **database, size_t *ptr_n) {
+DATABASE *read_and_add_line(DATABASE *database) {
   char user_input[BUFSIZ];
   printf("Enter message to add to database: ");
   // %[^\n]s will take a string with whitespaces (up to the first "enter")
   // whereas %s will only take the user input up to the first whitespace
   scanf("%[^\n]s", user_input);
 
-  database = add_message(user_input, database, ptr_n);
+  database = add_message(user_input, database);
 
   return database;
 }
 
 // d
-void print_all(char **database, size_t message_count) {
+void print_all(DATABASE *database) {
   printf("\033[1;mPrint all logs\033[0;m\n");
-  for (size_t idx = 0; idx < message_count; idx++) {
-    print_db_message(database, idx, message_count);
+  for (size_t idx = 0; idx < database->msg_count; idx++) {
+    print_db_message(idx, database);
   }
 }
-void free_all(char **database, size_t message_count) {
-  for (size_t idx = 0; idx < message_count; idx++) {
-    delete_last_message(database, &message_count);
+void free_all(DATABASE *database) {
+  for (size_t idx = 0; idx < database->msg_count; idx++) {
+    database = delete_last_message(database);
   }
 }
-void delete_db(char **database, size_t message_count) {
-  free_all(database, message_count);
-  free_database(database);
-}
+void delete_db(DATABASE *database) { free_all(database); }
 
-void print_db_row(char **database, size_t db_index, size_t message_count,
-                  size_t longest_row) {
-  if ((message_count < 1) || (db_index > (message_count - 1))) {
+void print_db_row(size_t db_index, size_t longest_row, DATABASE *database) {
+  if ((database->msg_count < 1) || (db_index > (database->msg_count - 1))) {
     printf("\033[31;mCannot access database at position %zu!\033[0;m (db_index "
            "= %zu "
            "> message_count = %zu)\n",
            db_index, db_index,
-           message_count > 0 ? message_count - 1 : message_count);
+           database->msg_count > 0 ? database->msg_count - 1
+                                   : database->msg_count);
     return;
   }
   char row_delimiter[BUFSIZ] = "";
@@ -137,14 +136,14 @@ void print_db_row(char **database, size_t db_index, size_t message_count,
   char *cross_character = (db_index == 0 ? "┬" : "┼");
   printf("───%s─%s─\n"
          " %zu │ %s\n",
-         cross_character, row_delimiter, db_index, database[db_index]);
+         cross_character, row_delimiter, db_index, database->data[db_index]);
 }
 
-size_t longest_row(char **database, size_t message_count) {
+size_t longest_row(DATABASE *database) {
   size_t longest_row = 0;
 
-  for (size_t idx = 1; idx < message_count; idx++) {
-    if (strlen(database[idx - 1]) < strlen(database[idx])) {
+  for (size_t idx = 1; idx < database->msg_count; idx++) {
+    if (strlen(database->data[idx - 1]) < strlen(database->data[idx])) {
       longest_row = idx;
     }
   }
@@ -152,18 +151,18 @@ size_t longest_row(char **database, size_t message_count) {
   return longest_row;
 }
 
-void print_db(char **database, size_t message_count) {
-  if (message_count == 0) {
+void print_db(DATABASE *database) {
+  if (database->msg_count == 0) {
     printf("\033[1;mEmpty database!\033[0;m\n");
     return;
   }
   printf("\033[1;mPrint all logs\033[0;m\n");
 
-  char *longest_entry = database[longest_row(database, message_count)];
+  char *longest_entry = database->data[longest_row(database)];
   size_t l_row = strlen(longest_entry);
 
-  for (size_t idx = 0; idx < message_count; idx++) {
-    print_db_row(database, idx, message_count, l_row);
+  for (size_t idx = 0; idx < database->msg_count; idx++) {
+    print_db_row(idx, l_row, database);
   }
 }
 
@@ -172,7 +171,7 @@ int main() {
   database->data = new_database();
   database->msg_count = 0;
 
-  char **db = new_database();
+  // char **db = new_database();
   size_t msg_count = 0;
 
   printf("msg_count: %zu\n", msg_count);
@@ -188,23 +187,22 @@ int main() {
   // printf("*(db + 5): %s\n", *(db + 5)); // will seg-fault at this point bc
   // the database is not big enough
 
-  db = add_message("Hello", db, &msg_count);
-  db = add_message("World", db, &msg_count);
-  db = add_message("How", db, &msg_count);
-  db = add_message("Are", db, &msg_count);
-  db = add_message("You", db, &msg_count);
-  db = add_message("On", db, &msg_count);
+  // db = add_message("Hello", db, &msg_count);
+  // db = add_message("World", db, &msg_count);
+  // db = add_message("How", db, &msg_count);
+  // db = add_message("Are", db, &msg_count);
+  // db = add_message("You", db, &msg_count);
+  // db = add_message("On", db, &msg_count);
 
-  database->data = add_message("Hello", database->data, &database->msg_count);
-  database->data = add_message("World", database->data, &database->msg_count);
-  database->data = add_message("How", database->data, &database->msg_count);
-  database->data = add_message("Are", database->data, &database->msg_count);
-  database->data = add_message("You", database->data, &database->msg_count);
-  database->data = add_message("On", database->data, &database->msg_count);
-  print_db(database->data, database->msg_count);
-  print_db(db, msg_count);
+  database = add_message("Hello", database);
+  database = add_message("World", database);
+  database = add_message("How", database);
+  database = add_message("Are", database);
+  database = add_message("You", database);
+  database = add_message("On", database);
+  print_db(database);
 
-  delete_db(db, msg_count);
+  free_all(database);
   // Should produce seg-fault at this point
   // printf("*db: %s\n", *db);
   // printf("*(db + 1): %s\n", *(db + 0));
