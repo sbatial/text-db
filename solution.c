58dc47d45317e83f7fa0bb9ef5918e8fc146ab9b
sbatial <mail@kbode.com>
2023-06-05 22:22:49 +0200

    Return early if user tries to delete field outside of log range

--- solution.c
+++ solution.c
@@ -52,6 +52,11 @@ DATABASE *delete_message(DATABASE *database, size_t index) {
     return database;
   }
 
+  if (index > (database->msg_count)) {
+    printf("No field with index %zu.\n", index);
+    return database;
+  }
+
   --database->msg_count;
 
   if (database->msg_count == 0) {
